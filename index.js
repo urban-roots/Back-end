497410743283a3fbc8c17ad428e3053f13e5f5a3
gonzalogomez-lgtm <gonzalo.gomez@anima.edu.uy>
2026-06-08 20:19:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,30 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("Servidor corriendo en http://localhost:3000");
 });
+
+const articulos = [
+    {id:1,
+    titulo:"",
+    autor:"",
+    cuerpo:"",
+    sobreElAutor:""
+    }
+    {id:2,
+    titulo:"",
+    autor:"",
+    cuerpo:"",
+    sobreElAutor:""
+    }
+    {id:3,
+    titulo:"",
+    autor:"",
+    cuerpo:"",
+    sobreElAutor:""
+    }
+    {id:4,
+    titulo:"HISTORIA DEL ROCK URUGUAYO",
+    autor:"https://culturarock.video.blog/2024/05/04/historia-del-rock-uruguayo/",
+    cuerpo:"El desarrollo del rock uruguayo fue lento e inconstante, llegando a su consolidación definitiva a comienzos de los 2000, de la mano de bandas como La Vela Puerca y No Te Va Gustar",
+    sobreElAutor:""    
+    }
+]
