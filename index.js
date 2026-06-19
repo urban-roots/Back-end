f7d3552eced9daece14a7395375bc37570815798
Jair4x <Jair4x@gmail.com>
2026-06-18 21:12:38 -0300

    CORS + dotenv + Arreglo de array de artículos
    Faltan escribir los sobre el autor y adaptar/agregar el cuerpo del artículo de cada uno.

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({ origin: ["http://localhost:5500", "http://localhost:5501"] }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.send("Hola mundo");
 })
-
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
-});
 
 const articulos = [
     {
         id: 1,
         titulo: "Origen del Carnaval",
         autor: "Jairo Severo",
-        cuerpo: "Qué sé yo, por algún lugar habrá surgido, pa mí se le ocurrió a Sendic.",
-        sobreElAutor: "Un goloso"
+        cuerpo: "",
+        sobreElAutor: "Jairo es un ..."
     },
     {
         id: 2,
-        titulo: "Origen del Rap",
+        titulo: "Origen del Rock Uruguayo",
         autor: "Federico Gutierrez",
-        cuerpo: "Yeah, perdonen, kame hame ha. Después del tema del tetris viene el Dragon Ball Rap.",
-        sobreElAutor: "Fulvo"
+        cuerpo: "",
+        sobreElAutor: "Federico es un ..."
     },
     {
         id: 3,
-        titulo: "Origen de la cumbia y la plena",
-        autor: "Nicole Silva",
-        cuerpo: "Cómo quieres tú que te quiera cariñito, si de tu amor no me das ni un poquito.",
-        sobreElAutor: "La única mujer del grupo"
+        titulo: "Historia del Rock Uruguayo",
+        autor: "Gonzalo Gomez",
+        cuerpo: "",
+        sobreElAutor: "Gonzalo es un ..."
     },
     {
         id: 4,
-        titulo: "Historia del Rock Uruguayo",
-        autor: "Gonzalo Gomez",
-        cuerpo: "El desarrollo del rock uruguayo fue lento e inconstante, llegando a su consolidación definitiva a comienzos de los 2000, de la mano de bandas como La Vela Puerca y No Te Va Gustar",
-        sobreElAutor: "Un veterano medio gagá"
+        titulo: "Origen de la cumbia y la plena",
+        autor: "Nicole Silva",
+        cuerpo: "",
+        sobreElAutor: "Nicole es una ..."
     }
 ];
 
@@ -56,4 +59,9 @@ app.get('/articulos/:id', (req, res) => {
     }
 
     res.send(articulos[idIndex]);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
